9ab2064ac01227c86acb297712fd9896329d08ea
lieutandat <lieutandat@gmail.com>
2021-06-19 21:23:17 +0700

    Create polyfill.js

---
+++ css/polyfill.js
@@ -0,0 +1,13 @@
+<script>
+  // IE11 does not have support for CSS variables, so we have to polyfill them
+  if (!(((window || {}).CSS || {}).supports && window.CSS.supports('(--a: 0)'))) {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://cdn.jsdelivr.net/npm/css-vars-ponyfill@2';
+    script.onload = function() {
+      cssVars({});
+    };
+
+    document.getElementsByTagName('head')[0].appendChild(script);
+  }
+</script>
